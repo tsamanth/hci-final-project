dd418058a5ee5a36ec2b7489d986913d5bab593e
Fluffau <49350466+Fluffau@users.noreply.github.com>
2023-04-19 17:18:30 -0500

    allow uploading image to firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { getStorage, ref } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
 export const db = getDatabase(app);
 
 export default app;
--- src/pages/closet/ViewPage.js
+++ src/pages/closet/ViewPage.js
@@ -8,6 +8,12 @@ import { Button } from '@mui/material';
 import { buttonStyle } from '../../display';
 import { db } from '../../firebase';
 import { update, ref } from 'firebase/database';
+
+import app from '../../firebase';
+import { getAuth} from "firebase/auth";
+import { getStorage, ref as sRef, uploadBytes, uploadBytesResumable, getDownloadURL} from "firebase/storage";
+
+
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -27,14 +33,75 @@ export default function ViewPage(props) {
     const [modalOpen, setModalOpen] = useState(false);
     const handleModalOpen = () => setModalOpen(true);
     const handleModalClose = () => setModalOpen(false);
+
+    const storage = getStorage(app);
+    const auth = getAuth(app);
+    const [storeLink, setLink] = useState('');
+
+    //alert(imagesRef);
     const uploadItem = e => {
+        //uploading item
         const file = e.target.files[0];
         const imgUrl = window.URL.createObjectURL(file);
+
+        const metadata = {
+            contentType: 'image/jpeg'
+          };
+          
+          // Upload file and metadata to the object
+          const storageRef = sRef(storage, 'images/' + file.name);
+          const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+          let url = '';
+          
+          // Listen for state changes, errors, and completion of the upload.
+          uploadTask.on('state_changed',
+            (snapshot) => {
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+                case 'paused':
+                  console.log('Upload is paused');
+                  break;
+                case 'running':
+                  console.log('Upload is running');
+                  break;
+              }
+            }, 
+            (error) => {
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              switch (error.code) {
+                case 'storage/unauthorized':
+                  // User doesn't have permission to access the object
+                  break;
+                case 'storage/canceled':
+                  // User canceled the upload
+                  break;
+          
+                // ...
+          
+                case 'storage/unknown':
+                  // Unknown error occurred, inspect error.serverResponse
+                  break;
+              }
+            }, 
+            () => {
+              // Upload completed successfully, now we can get the download URL
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                setLink(downloadURL);
+              });
+            }
+          );
+
         const updates = {
             [props.type]: [...props.images, imgUrl],
         };
+
         update(ref(db, `users/${props.userId}/closet`), updates);
     };
+
     const removeItem = (url, type) => {
         const newImgs = props.images.filter(i => {
             return i != url;
